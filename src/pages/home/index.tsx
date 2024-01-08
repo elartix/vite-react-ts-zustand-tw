c4e74256ae15185099954117322a732230f1a14d
Vladimir Mechkauskas <vmechkauskas@gmail.com>
2024-01-08 19:24:45 +0200

    - hidden contact form on home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,29 +9,11 @@ import { Hero } from '@/pages/home/hero.tsx';
 import ContactForm from '@/modules/ContactForm.tsx';
 
 export const Home = memo<PropsWithChildren<{ className?: string }>>(function Home ({ className }) {
-  // const [count, setCount] = useState(0)
   // @ts-ignore
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     fetch('/api/users')
-      .then((res) => res.json())
-      .then((json) => {
-        console.log(json);
-        setUsers(json.users);
-      });
-  }, []);
-
-
-  useEffect(() => {
-    fetch('/api/auth/validation/username', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-      },
-      // body: JSON.stringify({ username: 'hello' })
-      body: JSON.stringify({ username: 'test' })
-    })
       .then((res) => res.json())
       .then((json) => {
         console.log(json);
