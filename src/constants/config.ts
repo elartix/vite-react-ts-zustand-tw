6e6fe08d1eaedc1d7c024935b0c70348a18cb923
Volodymyr Mechkauskas <vmechkauskas@gmail.com>
2026-02-04 10:25:08 +0200

    chore(env): update `config.ts` to replace `REACT_APP` vars with `VITE_APP` equivalents and add new `TITLE` property

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -4,16 +4,17 @@ import _ from 'lodash';
 // local dependencies
 
 const environment = {
-  SID: varString(process.env.REACT_APP_SID),
-  NAME: varString(process.env.REACT_APP_NAME),
-  DESCRIPTION: varString(process.env.REACT_APP_DESCRIPTION),
-  SHORT_NAME: varString(process.env.REACT_APP_SHORT_NAME),
-  DEBUG: varBoolean(process.env.REACT_APP_DEBUG),
-  VERSION: varString(process.env.REACT_APP_VERSION),
-  PRODUCTION: varBoolean(process.env.REACT_APP_PRODUCTION),
-  DATE_FORMAT: varString(process.env.REACT_APP_DATE_FORMAT),
-  TIME_FORMAT: varString(process.env.REACT_APP_TIME_FORMAT),
-  DATE_TIME_FORMAT: `${varString(process.env.REACT_APP_DATE_FORMAT) } ${ varString(process.env.REACT_APP_TIME_FORMAT)}`,
+  SID: varString(process.env.VITE_APP_SID),
+  NAME: varString(process.env.VITE_APP_NAME),
+  TITLE: varString(process.env.VITE_APP_TITLE),
+  DESCRIPTION: varString(process.env.VITE_APP_DESCRIPTION),
+  SHORT_NAME: varString(process.env.VITE_APP_SHORT_NAME),
+  DEBUG: varBoolean(process.env.VITE_APP_DEBUG),
+  VERSION: varString(process.env.VITE_APP_VERSION),
+  PRODUCTION: varBoolean(process.env.VITE_APP_PRODUCTION),
+  DATE_FORMAT: varString(process.env.VITE_APP_DATE_FORMAT),
+  TIME_FORMAT: varString(process.env.VITE_APP_TIME_FORMAT),
+  DATE_TIME_FORMAT: `${varString(process.env.VITE_APP_DATE_FORMAT) } ${ varString(process.env.VITE_APP_TIME_FORMAT)}`,
 };
 
 // NOTE addition ability to enable debugging
@@ -37,7 +38,7 @@ function varBoolean (value?: string) {
   return /^(true|1)$/i.test(value as string);
 }
 
- 
+
 // @ts-ignore
 function varArray (value?: string) {
   return value ? value.split(',') : void 0;
