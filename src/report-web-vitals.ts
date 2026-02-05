66ea8b4e8ae071e93d413257b390aa4452b15db5
Volodymyr Mechkauskas <vmechkauskas@gmail.com>
2026-02-05 08:51:57 +0200

    chore(core): add `reportWebVitals` utility to track and report web vitals

---
+++ src/report-web-vitals.ts
@@ -0,0 +1,19 @@
+import { type MetricType } from 'web-vitals';
+
+export interface ReportCallback {
+  (metric: MetricType): void;
+}
+
+const reportWebVitals = (onPerfEntry?: ReportCallback) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ onCLS, onINP, onFCP, onLCP, onTTFB }) => {
+      onCLS(onPerfEntry);
+      onINP (onPerfEntry);
+      onFCP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
